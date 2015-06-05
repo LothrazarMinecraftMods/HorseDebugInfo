55e0dbc3aa6986c34a5f8b7e93dadce32709c7e5
sam bassett <samson.bassett@gmail.com>
2015-06-05 01:18:37 -0700

    first build test, resolving build file issues

--- src/main/java/com/lothrazar/samsf3/ModScreenText.java
+++ src/main/java/com/lothrazar/samsf3/ModScreenText.java
@@ -20,7 +20,7 @@ public class ModScreenText
 	public static final String MODID = "samsf3";
 	public static final String TEXTURE_LOCATION = MODID + ":";
 	public static final String VERSION = "1.8-1.0.0";
-	public static final String NAME = "Builder's Screen Text";
+	public static final String NAME = "Sam's Screen Text";
 	@Instance(value = MODID)
 	public static ModScreenText instance;
 	public static Logger logger; 
