35d073ccd5c3cddf727b51a824ae06202fe965eb
sam bassett <samson.bassett@gmail.com>
2015-07-02 12:31:10 -0700

    screentext: remove unused code; only need one class file

--- src/main/java/com/lothrazar/samsf3/ModScreenText.java
+++ src/main/java/com/lothrazar/samsf3/ModScreenText.java
@@ -1,15 +1,24 @@
 package com.lothrazar.samsf3;
 
+import java.text.DecimalFormat;
+
 import org.apache.logging.log4j.Logger;   
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.passive.EntityHorse;
 import net.minecraft.util.StatCollector;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
   
 @Mod(modid = ModScreenText.MODID, useMetadata = true )  
 public class ModScreenText
@@ -17,8 +26,7 @@ public class ModScreenText
 	public static final String MODID = "samsf3";
 	@Instance(value = MODID)
 	public static ModScreenText instance;
-	public static Logger logger; 
-//	public static ConfigText cfg;
+	//public static Logger logger; 
    
 	public static String lang(String name)
 	{
@@ -27,18 +35,49 @@ public class ModScreenText
 	@EventHandler
 	public void onPreInit(FMLPreInitializationEvent event)
 	{ 
-		logger = event.getModLog();  
-		
-		//cfg = new ConfigText(new Configuration(event.getSuggestedConfigurationFile()));
-	   
-	 	DebugScreenText h = new DebugScreenText();
-
-    	FMLCommonHandler.instance().bus().register(h); 
-    	MinecraftForge.EVENT_BUS.register(h); 
+		//logger = event.getModLog();  
+	
+    	FMLCommonHandler.instance().bus().register(instance); 
+    	MinecraftForge.EVENT_BUS.register(instance); 
 	}
         
-	@EventHandler
-	public void onInit(FMLInitializationEvent event)
-	{    
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent
+	public void onRenderTextOverlay(RenderGameOverlayEvent.Text event)
+	{  
+		EntityPlayerSP player = Minecraft.getMinecraft().thePlayer; 
+	
+		if(Minecraft.getMinecraft().gameSettings.showDebugInfo)
+		{
+		 	if(player.ridingEntity != null && player.ridingEntity instanceof EntityHorse)
+		 	{ 
+		 		addHorseInfo(event, player, (EntityHorse)player.ridingEntity);   
+		 	} 
+		}  
+	}
+
+	@SideOnly(Side.CLIENT)
+	private void addHorseInfo(RenderGameOverlayEvent.Text event,	EntityPlayerSP player,EntityHorse horse) 
+	{ 
+		double speed =  horse.getEntityAttribute(SharedMonsterAttributes.movementSpeed).getAttributeValue() ;
+
+		double jump = horse.getHorseJumpStrength() ;
+		//convert from scale factor to blocks
+		double jumpHeight = 0;
+		double gravity = 0.98;
+		while (jump > 0)
+		{
+			jumpHeight += jump;
+			jump -= 0.08;
+			jump *= gravity;
+		}
+		
+		DecimalFormat df = new DecimalFormat("0.0000");
+		
+		event.left.add(ModScreenText.lang("debug.horsespeed")+"  "+ df.format(speed)   ); 
+		
+		df = new DecimalFormat("0.0");
+		
+		event.left.add(ModScreenText.lang("debug.horsejump") +"  "+ df.format(jumpHeight) );
 	}
 }
